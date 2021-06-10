ebe514b8addb5f0351d62fc8d64130b689c32245
Aan Sumanto <aansumanto0210@gmail.com>
2021-06-10 16:46:12 +0700

    Add Implemetation Chache

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,23 +1,18 @@
+import CacheHelper from "./utils/cache-helper";
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event)=> {
     console.log('Installing Service Worker...');
-    
-    
-    // TODO: Caching App Shell Resource
-
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
     console.log('Activating Service Worker');
-
-
-    //TODO: Delete Old Caches
-
+    event.waitUntil(CacheHelper.deleteOldCache());
 })
 
 self.addEventListener('fetch', (event) => {
     console.log(event.request);
-
-
-    event.responWith(fetch(event.request));
-    //TODO: Add/Get fetch request to/from caches
+    event.respondWith(CacheHelper.revalidateCache(event.request));
 });
